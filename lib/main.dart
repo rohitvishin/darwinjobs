093eb564c561edef5f6035f5b1b190c7d66fd627
vaarohit <rohit.vish@darwinpgc.com>
2021-07-23 09:49:53 +0530

    uploading setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+void main()=> runApp(ShowList());
+
+class ShowList extends StatefulWidget {
+  const ShowList({ Key? key }) : super(key: key);
+
+  @override
+  _ShowListState createState() => _ShowListState();
+}
+void search(){
+
+}
+class _ShowListState extends State<ShowList> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+            
+            appBar: AppBar(
+              backgroundColor: Colors.greenAccent.shade700,
+              actions: [
+                IconButton(onPressed: search, icon: Icon(Icons.search),)
+              ],
+              title: Text("Job Search",style: GoogleFonts.sourceSansPro(fontSize: 18,color: Colors.white),),
+            ),
+            drawer: Drawer(
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text("Home"),
+                  ),
+                  ListTile(
+                    title: Text("Profile"),
+                  ),
+                  ListTile(
+                    title: Text("Logout"),
+                  ),
+                ],
+              ),
+            ),
+            body: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(30),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                  ],
+                  
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Designer Job",style: GoogleFonts.sourceSansPro(fontSize: 15,color: Colors.grey),),
+                        ]
+                      ),
+                      SizedBox(width: 210,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children:
+                        [
+                          Text("20th August 2021",style: GoogleFonts.sourceSansPro(fontSize: 12,color: Colors.greenAccent.shade700),),
+                        ]
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Vagator, Goa",style: GoogleFonts.sourceSansPro(fontSize: 12,color: Colors.grey),),
+                          
+                        ]
+                      ),
+                      SizedBox(width: 230,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children:
+                        [
+                          Text("CV Sent",style: GoogleFonts.sourceSansPro(fontSize: 12,color: Colors.grey),),
+                        ]
+                      )
+                    ],
+                  ),
+                    ],
+                  ),
+                  
+                ),
+              ],
+            ),
+           ),
+      ),
+    );
+  }
+}
